19409bc3a6bc92c658452eb43d1185ae1c23a31f
nbspi-HARLEY <daveharley875@gmail.com>
2024-02-15 18:26:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/petrack_sys/homepage.java
@@ -0,0 +1,17 @@
+package com.example.petrack_sys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class homepage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+    }
+}
